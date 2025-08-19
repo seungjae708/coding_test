95f97bc052232a35e5c051b0bd7fdbe9d97dcffd
seungjae708 <64647590+seungjae708@users.noreply.github.com>
2025-08-19 21:30:57 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long A,B,C;
+		A = scanner.nextLong();
+		B = scanner.nextLong();
+		C = scanner.nextLong();
+		System.out.println(A+B+C);
+	}
+}
