6ac82cc7bc04d7e23e8043c7cf94c1f9f019f95f
seungjae708 <64647590+seungjae708@users.noreply.github.com>
2025-08-19 21:28:44 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String a;
+		a = scanner.next();
+		System.out.println(a+"??!");
+	}
+
+}
