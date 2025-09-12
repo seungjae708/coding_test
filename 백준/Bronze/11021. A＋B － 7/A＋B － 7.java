bd0ca3372615171985cb1e926565a3da742ec294
seungjae708 <64647590+seungjae708@users.noreply.github.com>
2025-09-12 23:03:51 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18756 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n,a,b;
+		n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			int sum=0;
+			a=sc.nextInt();
+			b=sc.nextInt();
+			sum=a+b;
+			System.out.println("Case #"+(i+1)+": "+sum);
+		}
+	}
+}
