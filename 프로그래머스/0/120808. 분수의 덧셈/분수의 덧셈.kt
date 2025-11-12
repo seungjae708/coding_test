02f676b664f6d00a1f37cabc8d0a99d12551a809
Choi Seungjae <64647590+seungjae708@users.noreply.github.com>
2025-11-12 18:46:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
@@ -1,19 +1,9 @@
 class Solution {
     fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
-        var finalDenom = denom1 * denom2 
+        var finalDenom = denom1 * denom2
         var finalNumer = numer1 * denom2 + numer2 * denom1
         val finalGcd = gcd(finalNumer, finalDenom)
         return intArrayOf(finalNumer/finalGcd, finalDenom/finalGcd)
     }
-    
-    private fun gcd(a: Int, b: Int): Int{
-        var num1 = a
-        var num2 = b
-        while (num2 != 0){
-            val temp = num2
-            num2 = num1 % num2 
-            num1 = temp
-        }
-        return num1
-    }
+    private fun gcd(a: Int, b:Int): Int = if (b != 0) gcd(b, a % b) else a
 }
